61e8765aedecf53af1a21da4f21cd60a2c7ccb04
Asifali <asifalisayed19@gmail.com>
2019-12-01 17:14:21 +0530

    Added my PC-repair Application Project

---
+++ routes/contact.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer=require('nodemailer');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+//Send email
+router.post('/send',function(req,res,next){
+  var transporter=nodemailer.createTransport({
+    service:'Gmail',
+    auth:{
+      user:'asifalisayed19@gmail.com',
+      pass:'xwcavwsczrksbgsy'
+    }
+  });
+  var mailOptions={
+    from:'"Sayed Asifali" <asifalisayed19@gmail.com>',
+    to:'asifalisayed19@gmail.com',
+    subject:'Hello from PCRepair',
+    text:'You have a submission from ... Name:'+req.body.name+'Email:'+req.body.email+'Message:'+req.body.message,
+    html:'<p>You have a submission from ...</p> <ul><li>Name:'+req.body.name+'</li><li>Email:'+req.body.email+'</li><li>Message:'+req.body.message+'</li></ul>'
+  }
+  transporter.sendMail(mailOptions,function(error,info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message Sent:'+info.response);
+    res.redirect('/');
+  })
+});
+
+module.exports = router;
